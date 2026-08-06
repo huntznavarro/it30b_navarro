bb5eb5fb17bc1f4e79a5a0fbc4fa6238debb9071
huntznavarro <huntzorravan@gmail.com>
2026-08-06 17:23:42 +0800

    delete and doble

--- navarro.sql
+++ navarro.sql
@@ -5,3 +5,36 @@
         );
 
 create table booking (booking_id int (10));
+
+
+
+
+
+    CREATE TABLE student (
+    Student_id int(11) NOT NULL,
+    First_Name varchar(50) DEFAULT NULL,
+    Last_Name varchar(50) DEFAULT NULL,
+    Age int(10) DEFAULT NULL
+    );
+
+
+
+    CREATE TABLE borrow (
+    Borrow_ID int(11) NOT NULL,gio
+    Student_ID int(10) NOT NULL,
+    Book_ID int(10) NOT NULL,
+    Borrow_Date date NOT NULL,
+    Due_Date date NOT NULL,
+    Return_Date date DEFAULT NULL,
+    Status varchar(255) DEFAULT NULL
+    );
+
+
+
+
+    CREATE TABLE books (
+    Book_ID int(11) NOT NULL,
+    Title varchar(255) DEFAULT NULL,
+    Author varchar(255) DEFAULT NULL,
+    Published_Year date DEFAULT NULL
+    );
