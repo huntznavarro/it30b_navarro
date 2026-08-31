727eded250d0b0de3cb9fcc17730f3d022cf90f1
huntznavarro <huntzorravan@gmail.com>
2026-08-31 10:32:12 +0800

    fetch all the student and book info

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
+require_once 'config/config.php';
+require_once 'includes/activity_logger.php';
+
+$students = $pdo->query("
+    SELECT student_id, student_first_name, student_last_name
+    FROM students
+")->fetchALL(PDO::FETCH_ASSOC);
 
 
+$books = $pdo->query("
+    SELECT book_id, book_title
+    FROM books
+")->fetchALL(PDO::FETCH_ASSOC);
 
 
 
